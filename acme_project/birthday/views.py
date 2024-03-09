56ab341bd544990d234a334630a759a8b10f619f
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-03-09 13:08:19 +0300

    lesson 7 part 1

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,12 +4,10 @@ from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
-    # Создаём словарь контекста сразу после инициализации формы.
+    form = BirthdayForm(request.POST or None)
     context = {'form': form}
-    # Если форма валидна...
     if form.is_valid():
-        # ...вызовем функцию подсчёта дней:
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
@@ -17,27 +15,3 @@ def birthday(request):
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
-'''
-def birthday(request):
-    if request.GET:
-        form = BirthdayForm(request.GET)
-        if form.is_valid():
-            pass
-    else:
-        form = BirthdayForm()
-    context = {'form': form}
-    return render(request, 'birthday/birthday.html', context)
-'''
-
-'''def birthday(request):
-    form = BirthdayForm(request.GET or None)
-    if form.is_valid():
-        pass
-    context = {'form': form}
-    return render(request, 'birthday/birthday.html', context) 
-Этот код гораздо короче, а работает точно так же, как и предыдущий вариант.
-Весь фокус в выражении BirthdayForm(request.GET or None). 
-Его логика такова: если в GET-запросе были переданы параметры — значит,
-объект request.GET не пуст и этот объект передаётся в форму;
-если же объект request.GET пуст — срабатывает условиe or и форма создаётся без параметров,
-через BirthdayForm(None) — это идентично обычному BirthdayForm().'''
