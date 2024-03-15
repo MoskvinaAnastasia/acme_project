49ba7ccb6401d236bfacf971917b7b68af3aeef9
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-03-15 11:22:09 +0300

    Поздравления: комментирование записей

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .validators import real_age
 from django.core.exceptions import ValidationError
-from .models import Birthday
+from .models import Birthday, Congratulation
 from django.core.mail import send_mail
 
 
@@ -67,4 +67,11 @@ class BirthdayForm(forms.ModelForm):
             )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-            ) 
+            )
+
+ 
+class CongratulationForm(forms.ModelForm):
+    
+    class Meta:
+        model = Congratulation
+        fields = ('text',) 
